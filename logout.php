ea11b66515ce3915689c065930e14db53ee7a37e
Josef Weslley Batista dos santos <30638789+josefizuda@users.noreply.github.com>
2025-10-30 17:30:00 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,11 +4,7 @@ require_once 'config.php';
 $auth = new Auth();
 
 if (isset($_COOKIE['sso_token'])) {
-    $session = $auth->validateSession($_COOKIE['sso_token']);
-    if ($session) {
-        $auth->logAudit($session['user_id'], 'logout');
-        $auth->destroySession($_COOKIE['sso_token']);
-    }
+    $auth->destroySession($_COOKIE['sso_token']);
     $auth->clearSessionCookie();
 }
 
